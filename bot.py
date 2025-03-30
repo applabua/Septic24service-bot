fc1ac3760862fdd9a34a298f2c4670132222a641
AppLab📱 <083molonlabe@gmail.com>
2025-03-30 14:57:09 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -41,8 +41,8 @@ from aiohttp import web
 print("Бот працює...")
 
 # --- Настройки ---
-TOKEN = "ВАШ_TELEGRAM_BOT_TOKEN"  # Замените на ваш токен бота
-CHAT_ID = "ВАШ_CHAT_ID"            # Замените на ваш ID администратора
+TOKEN = "7747992449:AAEqWIUYRlhbdiwUnXqCYV3ODpNX9VUsed8"  # Замените на ваш токен бота
+CHAT_ID = "2045410830"            # Замените на ваш ID администратора
 
 # Словарь для бонус-счётчиков (не сохраняется между перезапусками)
 bonus_counters = {}
